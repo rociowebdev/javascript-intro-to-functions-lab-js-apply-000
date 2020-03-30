ba32ad9032722924bd99929b5a33bb37031aa910
Rocio C <rociocordero@gmail.com>
2020-03-30 01:31:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 function shout(string) {
   return string.toUpperCase()
-  
-}
-function whisper(string) {return string.toLowerCase()}
-function logShout(string) {console.log(string.toUpperCase())}
-function logWhisper(string) {console.log(string.toLowerCase())}
+  }
+function whisper(string) {
+  return string.toLowerCase()
+  }
+function logShout(string) {
+  console.log(string.toUpperCase())
+  }
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+  }
 function sayHiToGrandma(string) {
     if (string.toLowerCase() === string){
     return "I can't hear you!";
